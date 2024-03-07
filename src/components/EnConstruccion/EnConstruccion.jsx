8ae53663efe32a61388aae55c4d722e18c072382
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-03-07 14:15:30 +0100

    Versión 1.11 Pequeñas modificaciones CSS y Github link

--- src/components/EnConstruccion/EnConstruccion.jsx
+++ src/components/EnConstruccion/EnConstruccion.jsx
@@ -5,6 +5,9 @@ function EnConstruccion()
         <>
             <div>
                 <div>
+                <ul> <h3> Mi Github </h3>
+                    <a href="https://github.com/demilv">Mis proyectos</a>
+                </ul>
                     <ul> <h3>Elementos en construcción</h3>
                         <li> Modo oscuro</li>
                         <li> Login y rutas protegidas</li>
@@ -16,7 +19,10 @@ function EnConstruccion()
                 <div>
                     <ul><h3> Versiones </h3>
                     <h4>Version 1.1</h4>
-                    <li> Corregidos algunos problemas por los cuales las habitaciones de paciente no se quedaban libres al eliminar los pacientes o cambiarlos a nuevas habitaciones</li></ul>
+                    <li> Corregidos algunos problemas por los cuales las habitaciones de paciente no se quedaban libres al eliminar los pacientes o cambiarlos a nuevas habitaciones</li>
+                    <h4>Version 1.11</h4>
+                    <li> Añadido enlace a Github y algunas modificaciones del CSS</li>
+                    </ul>
                 </div>
             </div>
         </>
