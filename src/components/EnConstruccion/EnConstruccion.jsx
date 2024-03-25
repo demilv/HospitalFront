4919f8180961b01188eadb8c0273011db0cc09c8
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-03-25 19:13:22 +0100

    Minimo cambio al apartado en construccion

--- src/components/EnConstruccion/EnConstruccion.jsx
+++ src/components/EnConstruccion/EnConstruccion.jsx
@@ -9,8 +9,6 @@ function EnConstruccion()
                     <a href="https://github.com/demilv">Mis proyectos</a>
                 </ul>
                     <ul> <h3>Elementos en construcción</h3>
-                        <li> Modo oscuro</li>
-                        <li> Login y rutas protegidas</li>
                         <li> Posibles mejoras adicionales a elementos ya existentes</li>
                         <li> Responsive</li>
                         <li> ¿Otros?</li>
